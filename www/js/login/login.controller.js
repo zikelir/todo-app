0cb026db2007bf2fe5454f671d8fabf8e1ce77c7
PV Campelo Selano <pv_sf@hotmail.com>
2017-03-05 15:50:36 -0300

    'trying'

--- www/js/login/login.controller.js
+++ www/js/login/login.controller.js
@@ -1,10 +1,48 @@
-angular.module('login.module', ['ui.router']);
+angular.module('login.module', ['ionic', 'ui.router']);
+
 angular.module('login.module')
-.controller('loginCtrl', function ($scope, Platform) { 
-     Platform.ready(function() {
-    // Hide the status bar
-    StatusBar.hide();
-  });
+.controller('loginCtrl', function ($scope) { 
+     
+// //hide navbar on this view     
+//   Platform.ready(function() {
+//     // Hide the status bar
+//     StatusBar.hide();
+//   });
+
+
+  // Form data for the login modal
+  $scope.loginData = {
+      username: "abc",
+      password: "abc"
+  };
+
+  // Create the login modal that we will use later
+  // $ionicModal.fromTemplateUrl('js/login/login.html', {
+  //   scope: $scope
+  // }).then(function(modal) {
+  //   $scope.modal = modal;
+  // });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
 
  });
 
